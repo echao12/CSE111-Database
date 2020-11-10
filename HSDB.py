7025d859c3ddf12b3f6eb441e1aa11d865762488
ChaoTime <echao0077@gmail.com>
2020-11-10 01:31:38 -0800

    created and populated minions, spells, weapons tables
    fn create_tables_from_data() will generate and populate all tables based off of the heroes/classes/cards .csv files.

--- HSDB.py
+++ HSDB.py
@@ -78,7 +78,7 @@ class HSDB:
         # TODO: Create and populate tables: cards, minions, spells, weapons, classes, heroes, classcards
         print("Creating Cards Table\n")
         try:
-            sql = """CREATE TABLE Cards (
+            sql = """CREATE TABLE cards (
                 card_key INTEGER PRIMARY KEY AUTOINCREMENT,
                 card_name varchar(25) not null,
                 card_cost integer,
@@ -91,12 +91,12 @@ class HSDB:
             self.conn.rollback()
             print(e)
 
-        print("Creating Class Table")
+        print("Creating class Table")
         try:
             args = ["class_key integer primary key autoincrement","class_name varchar(15) not null"]
             #self.create_table("Classes", "class_key primary key autoincrement, class_name varchar(15) not null")
-            self.create_table("Classes", args)
-            print("Populating Classes Table")
+            self.create_table("classes", args)
+            print("Populating classes Table")
             try:
                 with open('data/classes.csv', 'r') as classData:
                     classReader = csv.reader(classData, quoting=csv.QUOTE_ALL, skipinitialspace=True)
@@ -106,7 +106,7 @@ class HSDB:
                     #print(sql)
                     #note: classes csv format: class_name
                     for row in classReader:
-                        sql = """INSERT INTO {} (class_name) VALUES(?)""".format("Classes")
+                        sql = """INSERT INTO {} (class_name) VALUES(?)""".format("classes")
                         #print(sql)
                         args = [row[0]]
                         #print("before execute")
@@ -121,14 +121,14 @@ class HSDB:
                 print(e)
 
         except Error as e:
-            print("Error Creating/Populating Class Table")
+            print("Error Creating/Populating class Table")
             print(e)
 
 
 
-        print ("Creating Heroes Table")
+        print ("Creating heroes Table")
         try:
-            sql = """CREATE TABLE Heroes (
+            sql = """CREATE TABLE heroes (
                 hero_classkey integer,
                 hero_name varchar(20) not null,
                 hero_power_name varchar(15) not null,
@@ -144,7 +144,7 @@ class HSDB:
             print(e)
         
         print("Starting to import data...")
-        print("Importing Cards data...")
+        print("Importing cards data...")
         try:
             with open('data/cards.csv', 'r') as cardData:
                 cardReader = csv.reader(cardData, quoting=csv.QUOTE_ALL, skipinitialspace=True)
@@ -153,14 +153,14 @@ class HSDB:
                 #cards csv format ['Name', 'Type', 'Rarity', 'Cost', 'Attack', 'Health', 'Text', 'Classes']
                 for row in cardReader:
                     #print(row)
-                    self.insertCardToTable("Cards", row[0], row[3], row[2], row[1])
-
-            print("Done Reading In Cards Data\n")
+                    self.insertCardToTable("cards", row[0], row[3], row[2], row[1])
+            cardData.close()
+            print("Done Reading In cards Data\n")
         except Error as e:
             self.conn.rollback()
             print(e)
         
-        print("Importing Heroes Data...")
+        print("Importing heroes Data...")
         try:
             with open('data/heroes.csv', 'r') as heroData:
                 heroReader = csv.reader(heroData, quoting=csv.QUOTE_ALL, skipinitialspace=True)
@@ -169,19 +169,172 @@ class HSDB:
                 #heroes csv format ['Name', 'Hero Power Name', 'Hero Power Cost', 'Hero Power Text', 'Class']
                 for row in heroReader:
                     #print(row)
-                    self.insertHeroToTable("Heroes", row[0], row[1], row[2], row[3], row[4])
-            print("Done reading/inserting Hero data...\n")
+                    self.insertHeroToTable("heroes", row[0], row[1], row[2], row[3], row[4])
+            heroData.close()
+            print("Done reading/inserting hero data...\n")
         except Error as e:
             print(e)
 
-        print("Creating all relational tables for Cards/Heroes...\n")
+        print("Creating all relational tables for cards/heroes...\n")
         print("Generating class_cards Table")
         try:
             self.generateClassCardsTable()
         except Error as e:
             print("ERROR in generating class_cards Table!")
             print(e)
+        try:
+            self.generateMinions()
+        except Error as e:
+            print("error generating minions Table")
+            print(e)
+        try:
+            self.generateSpells()
+        except Error as e:
+            print("Error generating spells table")
+            print(e)
+        try:
+            self.generateWeapons()
+        except Error as e:
+            print("Error generating weapons table")
+            print(e)
+        print("*** Finished generating all tables using data from the cards/classes/heroes csv files***\n")
         
+    def generateWeapons(self):
+        print("Begin generating weapons table...")
+        try:
+            self.create_table('weapons', ['weapon_cardkey integer', 'weapon_attack integer', 'weapon_durability integer', 'weapon_text varchar(25) not null'])
+        except Error as e:
+            print("Error creating weapons table...")
+            print(e)
+        try:
+            with open("data/cards.csv", 'r') as cardData:
+                cardReader = csv.reader(cardData, quoting=csv.QUOTE_ALL, skipinitialspace=True)
+                header = next(cardReader)
+                #cards csv format ['Name', 'Type', 'Rarity', 'Cost', 'Attack', 'Health', 'Text', 'Classes']
+                print(header)
+                for card in cardReader:
+                    #minions format: cardkey, attack, health, text
+                    if card[1] != 'Weapon':
+                        continue 
+                    else:
+                        try:
+                            sql = '''select card_key from cards where card_name = "{}"'''.format(card[0])
+                            cur = self.conn.cursor()
+                            cur.execute(sql)
+                            weapon_cardkey = cur.fetchone()
+                            weapon_cardkey = weapon_cardkey[0]
+                            weapon_attack = card[4]
+                            weapon_durability = card[5]
+                            weapon_text = card[6]
+                            sql = '''Insert INTO {}(weapon_cardkey, weapon_attack, weapon_durability, weapon_text) values (?,?,?,?)'''.format("weapons")
+                            args = [weapon_cardkey, weapon_attack, weapon_durability, weapon_text]
+                            #print(sql)
+                            self.conn.execute(sql, args)
+                            self.conn.commit()
+                        except Error as e:
+                            self.conn.rollback()
+                            print("Error inserting weapon {} into weapons table".format(card[0]))
+                            print(e)
+            cardData.close()
+            print("SUCCESS!")
+        except Error as e:
+            print("ERROR reading/inserting weapon cards into weapons table")
+        print("Completed weapons table generation...\n")
+
+
+
+
+
+
+    def generateSpells(self):
+        print("Begin generating spells table...")
+        try:
+            self.create_table('spells', ['spell_cardkey integer', 'spell_text varchar(25) not null'])
+        except Error as e:
+            print("Error creating spells table...")
+            print(e)
+        try:
+            with open("data/cards.csv", 'r') as cardData:
+                cardReader = csv.reader(cardData, quoting=csv.QUOTE_ALL, skipinitialspace=True)
+                header = next(cardReader)
+                #cards csv format ['Name', 'Type', 'Rarity', 'Cost', 'Attack', 'Health', 'Text', 'Classes']
+                print(header)
+                for card in cardReader:
+                    #minions format: cardkey, attack, health, text
+                    if card[1] != 'Spell':
+                        continue 
+                    else:
+                        try:
+                            sql = '''select card_key from cards where card_name = "{}"'''.format(card[0])
+                            cur = self.conn.cursor()
+                            cur.execute(sql)
+                            spell_cardkey = cur.fetchone()
+                            spell_cardkey = spell_cardkey[0]
+                            spell_text = card[6]
+                            sql = '''Insert INTO {}(spell_cardkey, spell_text) values (?,?)'''.format("spells")
+                            args = [spell_cardkey, spell_text]
+                            #print(sql)
+                            self.conn.execute(sql, args)
+                            self.conn.commit()
+                        except Error as e:
+                            self.conn.rollback()
+                            print("Error inserting spell {} into spells table".format(card[0]))
+                            print(e)
+            cardData.close()
+            print("SUCCESS")
+        except Error as e:
+            print("ERROR reading/inserting spells into spell table")
+        
+        print("Completed spells table generation...\n")
+
+
+    def generateMinions(self):
+        print("Begin generating minions table...")
+        try:
+            self.create_table('minions', ['minion_cardkey integer', 'minion_attack integer', 'minion_health integer', 'minion_text varchar(25) not null'])
+        except Error as e:
+            print("Error creating minions table...")
+            print(e)
+        try:
+            with open("data/cards.csv", 'r') as cardData:
+                cardReader = csv.reader(cardData, quoting=csv.QUOTE_ALL, skipinitialspace=True)
+                header = next(cardReader)
+                #cards csv format ['Name', 'Type', 'Rarity', 'Cost', 'Attack', 'Health', 'Text', 'Classes']
+                print(header)
+                for card in cardReader:
+                    #minions format: cardkey, attack, health, text
+                    if card[1] != 'Minion':
+                        continue 
+                    else:
+                        print(card)
+                        try:
+                            attack = card[4]
+                            health = card[5]
+                            text = card[6]
+                            sql = '''select card_key from Cards where card_name = "{}"'''.format(card[0])
+                            cur = self.conn.cursor()
+                            cur.execute(sql)
+                            cardkey = cur.fetchone()
+                            cardkey = cardkey[0]
+                            #print("cardkey:{} | health:{} | text:{} | {}".format(cardkey, health, attack, text))
+                            sql = '''INSERT INTO {}(minion_cardkey, minion_attack, minion_health, minion_text) values (?,?,?,?)'''.format('minions')
+                            args = [cardkey, attack, health, text]
+                            self.conn.execute(sql, args)
+                            self.conn.commit()
+                        except Error as e:
+                            print("ERROR inserting {} into minion table...".format(card[0]))
+                            self.conn.rollback()
+                            print(e)
+            cardData.close()
+            print("SUCCESS!")
+        except Error as e:
+            print(e)
+        print("Done generating minion table...\n")
+
+
+    
+                    
+
     def generateClassCardsTable(self):
         #will match up cardkey to corresponding classkey
         #must open cards.csv to get class name
@@ -201,7 +354,7 @@ class HSDB:
                     card_name = card[0]
                     card_classes = card[7].split("|")
                     #print("got card class/name")
-                    sql = '''select card_key from Cards where card_name = "{}"'''.format(card_name)
+                    sql = '''select card_key from cards where card_name = "{}"'''.format(card_name)
                     cur = self.conn.cursor()
                     cur.execute(sql) #get card_key from cards table
                     values = cur.fetchone()
@@ -209,7 +362,7 @@ class HSDB:
                     #print("got card key")
                     #print("Class Key Values:")
                     for card_class in card_classes:
-                        sql = '''select class_key from Classes where class_name = "{}"'''.format(card_class)
+                        sql = '''select class_key from classes where class_name = "{}"'''.format(card_class)
                         cur = self.conn.cursor()
                         cur.execute(sql)
                         card_class_val = cur.fetchone()
@@ -226,10 +379,13 @@ class HSDB:
                             self.conn.commit()
                         except Error as e:
                             print("Failed to insert class_cards entry for {}".format(card_name))
+                            print(e)
+                            self.conn.rollback()
             cardData.close()
+            print("SUCCESS!")
         except Error as e:
             print(e)
-        print("Finished generating class_card table!")
+        print("Finished generating class_card table!\n")
 
                     
                     
@@ -243,7 +399,7 @@ class HSDB:
         #print("Inserting hero {} to table...".format(hero_name))
         try:
             sql = """INSERT INTO {} (hero_classkey, hero_name, hero_power_name, hero_power_cost, hero_power_text) VALUES (?,?,?,?,?)""".format(table)
-            classKeyValSQL = """select class_key from Classes where class_name = '{}'""".format(hero_class)
+            classKeyValSQL = """select class_key from classes where class_name = '{}'""".format(hero_class)
             #print(sql)
             try:
                 cur = self.conn.cursor()
--- main.py
+++ main.py
@@ -10,15 +10,24 @@ def main():
     db = HSDB()
     db.connect('data/hs.sqlite')
 
-    # Create tables
-    #db.drop_table("Cards")
+    # Drop all tables
+    db.drop_table("Cards")
     db.drop_table("Classes")
     db.drop_table("Heroes")
-    #this will generate the base Heroes, Classes, Cards, and other related tables for use.
-    db.create_tables_from_data(cards, heroes)
+
     
+    db.drop_table("spells")
+    db.drop_table("minions")
+    db.drop_table("class_cards")
+    db.drop_table("weapons")
+    #db.generateMinions() #testing minion table generation
+    #db.generateSpells() #testing spell table generation
+    #db.generateWeapons() #testing weapon table generation
 
     # Do stuff
+    # this fn will generate and populate Heroes, Classes, Cards, and other related tables
+    # for use from the cards/classes/heroes csv files.
+    db.create_tables_from_data(cards, heroes)
 
 if __name__ == '__main__':
     main()
